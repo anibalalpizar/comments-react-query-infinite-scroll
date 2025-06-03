4e4276bb041e8240ea52e7d4257b078e967131dd
anibalalpizar <anibal.alpizar14@gmail.com>
2025-06-02 20:10:30 -0600

    Implement CommentsList component with loading, error handling, and pagination support

--- src/app/comments/CommentsList.tsx
+++ src/app/comments/CommentsList.tsx
@@ -1,5 +1,59 @@
 "use client"
 
+import { Loader2 } from "lucide-react"
+
+import { useComments } from "./use-comments-hooks"
+import { Button } from "@/components/ui/button"
+import { Comment } from "./Comment"
+
 export default function CommentsList() {
-  return <div>Show comments here</div>
+  const {
+    data,
+    isPending,
+    isError,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useComments()
+
+  const comments = data?.pages.flatMap((page) => page.comments)
+
+  if (isPending) {
+    return <Loader2 className="animate-spin mx-auto" />
+  }
+
+  return (
+    <div className="space-y-4">
+      {comments && comments.length > 0 && (
+        <>
+          <div className="space-y-3">
+            {comments.map((comment) => (
+              <Comment key={comment.id} comment={comment} />
+            ))}
+          </div>
+
+          <div className="flex justify-center my-4">
+            {hasNextPage && (
+              <Button
+                onClick={() => fetchNextPage()}
+                disabled={isFetchingNextPage}
+                className="px-4 py-2"
+              >
+                {isFetchingNextPage ? "Loading more..." : "Load more comments"}
+              </Button>
+            )}
+          </div>
+        </>
+      )}
+      {!isError && !comments?.length && (
+        <div className="text-center">No comments yet.</div>
+      )}
+      {isError && (
+        <div className="text-center text-red-500">
+          Error loading comments: {error.message}
+        </div>
+      )}
+    </div>
+  )
 }
